fba52d9e693b851aeb5a89d1cb89c0454ba333dc
crewmate23 <84353905+crewmate23@users.noreply.github.com>
2021-07-26 01:34:07 -0400

    JUnit 5 testing code

--- src/test/java/ucf/assignments/FileManagerTest.java
+++ src/test/java/ucf/assignments/FileManagerTest.java
@@ -17,5 +17,218 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FileManagerTest {
 
+    private FileManager fileManager = new FileManager();
 
+    private Inventory inventory = new Inventory();
+    private ObservableList<Item> items = FXCollections.observableArrayList();
+
+    private List<Item> dummyData = new ArrayList<>();
+
+    public void addDummyData(){
+        dummyData.add(new Item("MacBook Air", "983AR93JMB", new BigDecimal(1199.00)));
+        dummyData.add(new Item("iPad Air", "342AR894IP", new BigDecimal(699.00)));
+    }
+
+    public void inventoryRefresh(){
+        inventory.getItems().removeAll();
+
+        addDummyData();
+        inventory.addItem(dummyData.get(0));
+        inventory.addItem(dummyData.get(1));
+    }
+
+    @Test
+    void save_TSV_Test() {
+        //first initialize
+        inventoryRefresh();
+
+        //hardcode location
+        File file = new File("/Users/sathwika/Desktop/Inventory_Deegutla/Testing.txt");
+        String fileType = ".txt";
+
+        fileManager.save(file, fileType, inventory);
+
+        String actual ="";
+
+        try{
+            //read the file
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String line = "";
+            while((line = br.readLine()) != null){
+                actual += line + "\n"; //add the file values into the actual string
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //create an expected string with dummy item's value
+        String expected = """
+                Name\tSerial Number\tValue\t
+                MacBook Air\t983AR93JMB\t1199\t
+                iPad Air\t342AR894IP\t699\t
+                """;
+
+
+        assertEquals(expected , actual);
+    }
+
+    @Test
+    void save_HTML_Test() {
+        //first initialize
+        inventoryRefresh();
+
+        //hardcode location
+        File file = new File("/Users/sathwika/Desktop/Inventory_Deegutla/Testing.html");
+        String fileType = ".html";
+
+        fileManager.save(file, fileType, inventory);
+
+        String actual ="";
+
+        try{
+            //read the file
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String line = "";
+            while((line = br.readLine()) != null){
+                actual += line + "\n"; //add the file values into the actual string
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //create an expected string with dummy item's value
+        String expected = """
+                <!DOCTYPE html>
+                <html>
+                <body>
+                <table>
+                <tr>
+                <th>Name</th>
+                <th>Serial Number</th>
+                <th>Value</th>
+                </tr>
+                <tr>
+                <td>MacBook Air</td>
+                <td>983AR93JMB</td>
+                <td>1199</td>
+                </tr>
+                <tr>
+                <td>iPad Air</td>
+                <td>342AR894IP</td>
+                <td>699</td>
+                </tr>
+                </table>
+                </body>
+                </html>
+                """;
+
+        assertEquals(expected , actual);
+    }
+
+    @Test
+    void save_JSON_Test(){
+        //first initialize
+        inventoryRefresh();
+
+        //hardcode location
+        File file = new File("/Users/sathwika/Desktop/Inventory_Deegutla/Testing.json");
+        String fileType = ".json";
+
+        fileManager.save(file, fileType, inventory);
+
+        String actual ="";
+
+        try{
+            //read the file
+            BufferedReader br = new BufferedReader(new FileReader(file));
+
+            String line = "";
+            while((line = br.readLine()) != null){
+                actual += line + "\n"; //add the file values into the actual string
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //create an expected string with dummy item's value
+        String expected = """
+                {"inventory":[{"name":"MacBook Air","serial number":"983AR93JMB","value":"1199"},{"name":"iPad Air","serial number":"342AR894IP","value":"699"}]}
+                """;
+
+        assertEquals(expected , actual);
+    }
+
+    @Test
+    void load_TSV_Test() {
+        inventoryRefresh();
+
+        //hardcode location
+        File file = new File("/Users/sathwika/Desktop/Inventory_Deegutla/Testing.txt");
+        String fileType = ".txt";
+
+        fileManager.save(file, fileType, inventory);
+
+        ObservableList<Item> loadItems = fileManager.load(file, fileType);
+
+        boolean validate = true;
+        for(int i = 0; i < inventory.getItems().size(); i++){
+            if(!loadItems.get(i).getName().equals(inventory.getItems().get(i).getName()))
+                validate = false;
+        }
+
+        assertTrue(validate);
+    }
+
+    @Test
+    void load_HTML_Test() {
+        inventoryRefresh();
+
+        //hardcode location
+        File file = new File("/Users/sathwika/Desktop/Inventory_Deegutla/Testing.html");
+        String fileType = ".html";
+
+        fileManager.save(file, fileType, inventory);
+
+        ObservableList<Item> loadItems = fileManager.load(file, fileType);
+
+        boolean validate = true;
+        for(int i = 0; i < inventory.getItems().size(); i++){
+            if(!loadItems.get(i).getName().equals(inventory.getItems().get(i).getName()))
+                validate = false;
+        }
+
+        assertTrue(validate);
+    }
+
+    @Test
+    void load_JSON_Test() {
+        inventoryRefresh();
+
+        //hardcode location
+        File file = new File("/Users/sathwika/Desktop/Inventory_Deegutla/Testing.json");
+        String fileType = ".json";
+
+        fileManager.save(file, fileType, inventory);
+
+        ObservableList<Item> loadItems = fileManager.load(file, fileType);
+
+        boolean validate = true;
+        for(int i = 0; i < inventory.getItems().size(); i++){
+            if(!loadItems.get(i).getName().equals(inventory.getItems().get(i).getName()))
+                validate = false;
+        }
+
+        assertTrue(validate);
+    }
 }
--- src/test/java/ucf/assignments/InventoryTest.java
+++ src/test/java/ucf/assignments/InventoryTest.java
@@ -16,6 +16,195 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class InventoryTest {
 
+    private Inventory inventory = new Inventory();
+    private ObservableList<Item> items = FXCollections.observableArrayList();
 
+    private List<Item> dummyData = new ArrayList<>();
+
+    public void addDummyData(){
+        dummyData.add(new Item("MacBook Air", "983AR93JMB", new BigDecimal(1199.00)));
+        dummyData.add(new Item("MacBook Pro", "983PR203MB", new BigDecimal(1599.00)));
+        dummyData.add(new Item("iPad Air", "342AR894IP", new BigDecimal(699.00)));
+        dummyData.add(new Item("iPad Pro", "342PR932IP", new BigDecimal(899.00)));
+    }
+
+    @Test
+    void addItem_Test(){
+        addDummyData();
+
+        inventory.addItem(dummyData.get(0));
+        inventory.addItem(dummyData.get(1));
+        inventory.addItem(dummyData.get(2));
+        inventory.addItem(dummyData.get(3));
+
+        //expected dummyData, actual inventory
+        assertArrayEquals(dummyData.toArray(), inventory.getItems().toArray());
+    }
+
+    @Test
+    void removeItems_Test() {
+        //add dummy data to inventory
+        addDummyData();
+        inventory.addItem(dummyData.get(0));
+        inventory.addItem(dummyData.get(1));
+        inventory.addItem(dummyData.get(2));
+        inventory.addItem(dummyData.get(3));
+
+        //remove 'Pro'
+        items.add(dummyData.get(1));
+        items.add(dummyData.get(3));
+
+
+        inventory.removeItems(items);
+
+        dummyData.removeAll(items);
+
+        assertArrayEquals(dummyData.toArray(), inventory.getItems().toArray());
+    }
+
+    public void inventoryRefresh(){
+        inventory.getItems().removeAll();
+
+        addDummyData();
+        inventory.addItem(dummyData.get(0));
+        inventory.addItem(dummyData.get(1));
+        inventory.addItem(dummyData.get(2));
+        inventory.addItem(dummyData.get(3));
+    }
+
+
+    @Test
+    void editName_Test() {
+        inventoryRefresh();
+
+        //edit "MacBook Pro" to "MacBook Pro Max"
+        inventory.editName(dummyData.get(1), "MacBook Pro Max");
+
+        assertEquals("MacBook Pro Max", inventory.getItems().get(1).getName());
+    }
+
+    @Test
+    void editValue_Test() {
+        inventoryRefresh();
+
+        //edit MacBook Air's price "1199.00" to "1299.00"
+        inventory.editValue(dummyData.get(0), new BigDecimal(1299.00));
+
+        assertEquals(new BigDecimal(1299.00), inventory.getItems().get(0).getValue());
+    }
+
+    @Test
+    void editSerialNumber_Test() {
+        inventoryRefresh();
+
+        //edit iPad Air's serial number "342AR894IP" to "342AR498IP"
+        inventory.editSerialNumber(dummyData.get(2), "342AR498IP");
+
+        assertEquals("342AR498IP", inventory.getItems().get(2).getSerialNumber());
+    }
+
+    @Test
+    void checkSerialNumber_true_Test() {
+        inventoryRefresh();
+
+        //check existing serial number "342PR932IP"
+        assertTrue(inventory.checkSerialNumber("342PR932IP"));
+    }
+
+    @Test
+    void checkSerialNumber_false_Test() {
+        inventoryRefresh();
+
+        //check non-existing serial number "342PR932IP"
+        assertFalse(inventory.checkSerialNumber("9124232IKN"));
+    }
+
+    @Test
+    void findItemBySerial_Test() {
+        inventoryRefresh();
+
+        //find iPad Pro by "342PR932IP"
+        Item item = inventory.findItemBySerial("342PR932IP");
+
+        assertEquals("342PR932IP", item.getSerialNumber());
+    }
+
+    @Test
+    void findItemByName_Test() {
+        inventoryRefresh();
+
+        //find iPad Pro by "iPad Pro"
+        Item item = inventory.findItemByName("iPad Pro");
+
+        assertEquals("iPad Pro", item.getName());
+    }
+
+    @Test
+    void sortByName_Test(){
+        inventoryRefresh();
+
+        boolean validate = true;
+
+        //add to expected items list in accordance with name
+        items.add(dummyData.get(2));
+        items.add(dummyData.get(3));
+        items.add(dummyData.get(0));
+        items.add(dummyData.get(1));
+
+        ObservableList<Item> inventoryItems = inventory.sortByName();
+
+        for(int i = 0; i < inventoryItems.size(); i++){
+            if(!inventoryItems.get(i).getName().equals(items.get(i).getName()))
+                validate = false;
+        }
+
+        assertTrue(validate);
+
+    }
+
+    @Test
+    void sortBySerialNumber_Test(){
+        inventoryRefresh();
+
+        boolean validate = true;
+
+        //add to expected items list in accordance with name
+        items.add(dummyData.get(2));
+        items.add(dummyData.get(3));
+        items.add(dummyData.get(0));
+        items.add(dummyData.get(1));
+
+        ObservableList<Item> inventoryItems = inventory.sortBySerialNumber();
+
+        for(int i = 0; i < inventoryItems.size(); i++){
+            if(!inventoryItems.get(i).getSerialNumber().equals(items.get(i).getSerialNumber()))
+                validate = false;
+        }
+
+        assertTrue(validate);
+    }
+
+    @Test
+    void sortByValue_Test(){
+        inventoryRefresh();
+
+        boolean validate = true;
+
+        //add to expected items list in accordance with name
+        items.add(dummyData.get(2));
+        items.add(dummyData.get(3));
+        items.add(dummyData.get(0));
+        items.add(dummyData.get(1));
+
+        ObservableList<Item> inventoryItems = inventory.sortByValue();
+
+        for(int i = 0; i < inventoryItems.size(); i++){
+            if(!inventoryItems.get(i).getValue().equals(items.get(i).getValue()))
+                validate = false;
+        }
+
+        assertTrue(validate);
+
+    }
 
 }
